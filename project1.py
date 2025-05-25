acf95a5c81a596dc7ee0b9d0eb67f138d7a8631a
mohith899 <2027vgs.mohiths@velammalglobal.edu.in>
2025-05-25 11:54:22 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,10 @@
+correctnum=200
+while True:
+    guess=int(input("enter ur guess num:"))
+
+
+
+    if  correctnum==guess:
+         print("congrats u nailed it")
+    else:
+         print("try again")
